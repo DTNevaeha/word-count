d2ec506d910fe5700a76fa577f0dd4cd453d156f
DTNevaeha <DTNevaeha@gmail.com>
2022-07-21 17:05:11 -0500

    Initial commit of word counting application.

--- src/main/java/com/tlglearning/wordcount/WordCounter.java
+++ src/main/java/com/tlglearning/wordcount/WordCounter.java
@@ -5,30 +5,43 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public final class WordCounter {
+public class WordCounter {
 
-  private final Map<String, Integer> counts;
+  private final Map<String, Integer> counts = new HashMap<>();
 
-  public WordCounter(String text) {
-    String[] words = splitWords(text);
-    counts = Collections.unmodifiableMap(countWords(words));
-  }
+  private int totalWords;
 
   public Set<String> words() {
     return counts.keySet();
   }
 
-  @Override
-  public String toString() {
-    return counts.toString();
-  }
-
-  public int getCount(String word) {  //if someone searches a word that doesnt exist, return 0
+  public int get(String word) {  //if someone searches a word that doesnt exist, return 0
     return counts.getOrDefault(word, 0);
   }
 
   public Map<String, Integer> getCounts() {
-    return counts;
+    return Collections.unmodifiableMap(counts);
+  }
+
+  public void add(String text) { //tries to see if lines are just blank and skip it
+    String trimmedLine = text.trim();
+    if (!trimmedLine.isEmpty()) {
+      String[] words = splitWords(trimmedLine);
+      countWords(words);
+    }
+  }
+
+  public int size() {
+    return counts.size();
+  }
+
+  public int total() { //gets the total words within the article
+    return totalWords;
+  }
+
+  @Override
+  public String toString() {
+    return counts.toString();
   }
 
   String[] splitWords(String text) {
@@ -37,20 +50,14 @@ public final class WordCounter {
         .split("[\\W_]+"); //replaces non-word character or _ with a space
   }
 
-  Map<String, Integer> countWords(String[] words) {
-    Map<String, Integer> counts = new HashMap<>();
+  void countWords(String[] words) { //this is return type
     for (String word : words) {
       // DONE check if word is already present as a key in counts;
       //  if it's not present, add it to counts with a value of 1.
       //  otherwise, get the current value, add 1 to it, and update the map with the new value.
-      if (!counts.containsKey(word)) {
-        counts.put(word, 1);
-      } else {
-        int previousCount = counts.get(word);
-        counts.put(word, previousCount + 1);
-      }
+      counts.put(word, get(word) + 1);
+      totalWords++;
     }
-    return counts;
   }
 
 }
